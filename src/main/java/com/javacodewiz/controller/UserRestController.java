fb1908fce2fa587f697295b738db14a57983e774
javacodewiz <javacodewiz@gmail.com>
2024-12-21 22:49:19 +0530

    implement spring cache

--- src/main/java/com/javacodewiz/controller/UserRestController.java
+++ src/main/java/com/javacodewiz/controller/UserRestController.java
@@ -5,6 +5,9 @@ import com.javacodewiz.dto.UserDto;
 import com.javacodewiz.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -32,24 +35,28 @@ public class UserRestController {
     }
 
     @GetMapping("/{id}")
+    @Cacheable(cacheNames = "user",key = "#id")
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") long id)
     {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
     @GetMapping
+    @Cacheable(cacheNames = "user",key = "#ids")
     public ResponseEntity<List<UserDto>> getAllUsers()
     {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @PutMapping
+    @CachePut(cacheNames = "user",key = "#dto.id")
     public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto dto)
     {
         return ResponseEntity.ok(userService.updateUser(dto));
     }
 
     @DeleteMapping("/{id}")
+    @CacheEvict(cacheNames = "user",key = "#id")
     public ResponseEntity<String> deleteUser(@PathVariable("id") long id)
     {
         return  ResponseEntity.status(HttpStatus.OK).body(userService.deleteUser(id));
